fcd699cc5ab41db7fda618f41537f03cc1a6dd49
Tadej Pecar <tpecar95@gmail.com>
2021-01-16 13:35:49 +0100

    Reworked SchComponent and placement algorithm.
    Now splits grouped components into multiple rows of --width
    Different groups are separated by --spacing

--- comp.py
+++ comp.py
@@ -4,34 +4,10 @@ Used in rules.py
 """
 
 from dataclasses import dataclass
+from net import NetComponent
 import re
 from typing import Any
 import uuid
-
-@dataclass
-class PlacedSchComponent:
-    """
-    KiCad schematic component + labels,
-    translated to position specified at place() invocation
-    """
-
-    lib_symbol      : str # lib_symbols entry
-    labels          : str # global label entries
-    symbol          : str # symbol entry
-    symbol_instance : str # symbol_instance entry
-
-    x : float # Top left corner X at which the component was placed
-    y : float # Top left corner Y at which the component was placed
-
-    bounds : tuple[float, float] # Bounds of the placed component
-
-    def __str__(self):
-        # Get first line of property, for all properties, to give an idea of the structure
-        return '\n'.join([
-            re.search('\s+\((.*)', prop).group(0)
-            for prop
-            in (self.lib_symbol, self.labels, self.symbol, self.symbol_instance)
-        ])
 
 class SchComponent:
     """
@@ -77,7 +53,7 @@ class SchComponent:
         super().__init__()
 
         # Parameters specified by user
-        self.d, self.f, self.v = [re.compile(r) for r in rule]
+        self.designator, self.footprint, self.value = [re.compile(r) for r in rule]
         
         self.lib_symbol = lib_symbol
         self.label_tpls = label_tpls
@@ -85,93 +61,20 @@ class SchComponent:
         self.symbol_inst_tpls = symbol_inst_tpls
         self.bounds = bounds
 
-    def match(self, designator:str, footprint:str, value:str):
-        return self.d.match(designator) and self.f.match(footprint) and self.v.match(value)
-
-    def place(self, x:float, y:float, designator:str, value:str, connection:dict[str, str]):
-        """
-        Place component (translate, set designator + value, set labels to nets)
-        """
-        
-        # The component symbol instances (which we assume were placed relative to (0,0))
-        # are translated by (x, y).
-        pos_re = re.compile('\(at\s+(\S+)\s+(\S+)')
-        def move(match):
-            return f'(at {float(match.group(1))+x} {float(match.group(2))+y}'
-
-        # The symbol entry does not contain the actual designator, value
-        # (this is specified by symbol_instance) but contains the actual coordinate!
-        #
-        # symbol and symbol_instance are connected via uuid
-        #
-        # We need to generate a new, unique uuids per each place() call,
-        # since the schematic can have multiple instances of the same component.
-        #
-        uuids = {id : uuid.uuid4() for id in self.symbol_tpls.keys()}
-
-        # Label pin to actual net (specified in netlist) replacement
-        label_re = re.compile('\(global_label \"[^"]*\"')
-
-        label_tpls = [
-            label_re.sub(f'(global_label \"{connection[pin]}\"',
-                pos_re.sub(move, label_tpl)
-            )
-            for pin, label_tpl
-            in self.label_tpls.items()
-
-            # We generate the label only if its pin is connected to a net
-            if pin in connection
-        ]
-
-        uuid_re = re.compile('\(uuid \"([^"]*)\"')
-        def uuid_replace(match):
-            return f'(uuid "{uuids[match.group(1)]}"'
-
-        symbol_tpls = [
-            uuid_re.sub(uuid_replace,
-                pos_re.sub(move, symbol_tpl)
-            )
-            for symbol_tpl
-            in self.symbol_tpls.values()
-        ]
-
-        path_re = re.compile('\(path \"\/([^"]*)\"')
-        def path_replace(match):
-            return f'(path "/{uuids[match.group(1)]}"'
-        
-        designator_re = re.compile('\(reference \"[^"]*\"\)')
-        value_re = re.compile('\(value \"[^"]*\"\)')
-
-        symbol_inst_tpls = [
-            value_re.sub(f'(value \"{value}\")',
-                designator_re.sub(f'(reference \"{designator}\")',
-                    path_re.sub(path_replace, symbol_instance)
-                )
-            )
-            for symbol_instance
-            in self.symbol_inst_tpls.values()
-        ]
-
-        return PlacedSchComponent(
-            # lib_symbol (graphical representation) is left as-is
-            lib_symbol = self.lib_symbol,
-
-            # global label entries
-            labels = "\n".join(label_tpls),
-
-            # symbol entry
-            symbol = "\n".join(symbol_tpls),
-
-            # symbol_instances entry
-            symbol_instance = "\n".join(symbol_inst_tpls),
-
-            x = x, y = y, bounds=self.bounds
-        )
+    def match(self, net_comp: NetComponent):
+        if (
+            self.designator.match(net_comp.designator) and
+            self.footprint.match(net_comp.footprint) and
+            self.value.match(net_comp.value)
+        ):
+            return MatchedSchComponent(self, net_comp)
+        else:
+            return None
     
     def __str__(self):
         # Quick and dirty description of the object
         symbol_lib_name = re.findall('\(symbol \"([^"]*)" ', self.lib_symbol)
-        return f'{symbol_lib_name} Rules [Designator "{self.d.pattern}" Footprint "{self.f.pattern}" Value "{self.v.pattern}"] Bounds {self.bounds}'
+        return f'{symbol_lib_name} Rules [Designator "{self.designator.pattern}" Footprint "{self.footprint.pattern}" Value "{self.value.pattern}"] Bounds {self.bounds}'
 
     @classmethod
     def loadFromFile(cls, sch_file_path : str) -> Any:
@@ -227,11 +130,11 @@ class SchComponent:
         # Sanity check that we got all symbols / instances
         # Should pass unless the schematic is malformed / regexes failed
         for uuid in symbol_tpls.keys():
-            if(uuid not in symbol_inst_tpls):
+            if uuid not in symbol_inst_tpls:
                 raise Exception(f'Internal: symbol uuid {uuid} missing in symbol_insts')
 
         for uuid in symbol_inst_tpls.keys():
-            if(uuid not in symbol_tpls):
+            if uuid not in symbol_tpls:
                 raise Exception(f'Internal: symbol_insts uuid {uuid} missing in symbol')
 
         # Extract bounds
@@ -251,6 +154,106 @@ class SchComponent:
             symbol_inst_tpls = symbol_inst_tpls,
             bounds = bounds
         )
+
+@dataclass
+class MatchedSchComponent:
+
+    sch_comp : SchComponent  # SchComponent (template) which matched
+    net_comp : NetComponent  # NetComponent (instance) which matched
+
+    def place(self, pos: tuple[float, float]):
+        """
+        Place component (translate, set designator + value, set labels to nets)
+        """
+        
+        # The component symbol instances (which we assume were placed relative to (0,0))
+        # are translated by (x, y).
+        pos_re = re.compile('\(at\s+(\S+)\s+(\S+)')
+        def move(match):
+            return f'(at {float(match.group(1)) + pos[0]} {float(match.group(2)) + pos[1]}'
+
+        # The symbol entry does not contain the actual designator, value
+        # (this is specified by symbol_instance) but contains the actual coordinate!
+        #
+        # symbol and symbol_instance are connected via uuid
+        #
+        # We need to generate a new, unique uuids per each place() call,
+        # since the schematic can have multiple instances of the same component.
+        #
+        uuids = {id : uuid.uuid4() for id in self.sch_comp.symbol_tpls.keys()}
+
+        # Label pin to actual net (specified in netlist) replacement
+        label_re = re.compile('\(global_label \"[^"]*\"')
+
+        rendered_labels = "\n".join([
+            label_re.sub(f'(global_label \"{self.net_comp.connections[pin]}\"',
+                pos_re.sub(move, label_tpl)
+            )
+            for pin, label_tpl
+            in self.sch_comp.label_tpls.items()
+
+            # We generate the label only if its pin is connected to a net
+            if pin in self.net_comp.connections
+        ])
+
+        uuid_re = re.compile('\(uuid \"([^"]*)\"')
+        def uuid_replace(match):
+            return f'(uuid "{uuids[match.group(1)]}"'
+
+        rendered_symbol = "\n".join([
+            uuid_re.sub(uuid_replace,
+                pos_re.sub(move, symbol_tpl)
+            )
+            for symbol_tpl
+            in self.sch_comp.symbol_tpls.values()
+        ])
+
+        path_re = re.compile('\(path \"\/([^"]*)\"')
+        def path_replace(match):
+            return f'(path "/{uuids[match.group(1)]}"'
+        
+        designator_re = re.compile('\(reference \"[^"]*\"\)')
+        value_re = re.compile('\(value \"[^"]*\"\)')
+
+        rendered_symbol_inst = "\n".join([
+            value_re.sub(f'(value \"{self.net_comp.value}\")',
+                designator_re.sub(f'(reference \"{self.net_comp.designator}\")',
+                    path_re.sub(path_replace, symbol_instance)
+                )
+            )
+            for symbol_instance
+            in self.sch_comp.symbol_inst_tpls.values()
+        ])
+
+        return PlacedSchComponent(
+            self,
+            rendered_labels=rendered_labels,
+            rendered_symbol=rendered_symbol,
+            rendered_symbol_inst=rendered_symbol_inst,
+            pos=pos
+        )
+
+@dataclass
+class PlacedSchComponent:
+    """
+    KiCad schematic component + labels,
+    translated to position specified at place() invocation
+    """
+    match : MatchedSchComponent
+
+    rendered_labels      : str # global label entries
+    rendered_symbol      : str # symbol entry
+    rendered_symbol_inst : str # symbol_instance entry
+
+    pos: tuple[float, float]
+
+    def __str__(self):
+        # Get first line of property, for all properties, to give an idea of the structure
+        return '\n'.join([
+            re.search('\s+\((.*)', prop).group(0)
+            for prop
+            in (self.lib_symbol, self.rendered_labels, self.rendered_symbol, self.rendered_symbol_inst)
+        ])
 
 
 if __name__ == '__main__':
@@ -274,10 +277,21 @@ if __name__ == '__main__':
     )
     args = parser.parse_args(sys.argv[1:])
 
-    comp = SchComponent.loadFromFile(args.kicad_sch)
-    print(comp)
+    sch_comp = SchComponent.loadFromFile(args.kicad_sch_path)
+    print(sch_comp)
 
-    placed_comp = comp.place(0, 0, 'test', 'test', {})
+    # Create a dummy NetComponent, MatchedSchComponent
+    net_comp = NetComponent(
+        designator='test_designator',
+        footprint='test_footprint',
+        value='test_value'
+    )
+    matched_comp = MatchedSchComponent(
+        sch_comp,
+        net_comp
+    )
+
+    placed_comp = matched_comp.place((0, 0))
 
     # If target file is provided, recreate schematic with placed component
     # (Since the net dict is empty, no labels will be created)
@@ -290,18 +304,18 @@ f'''
   (paper "A4")
 
   (lib_symbols
-{placed_comp.lib_symbol}
+{placed_comp.match.sch_comp.lib_symbol}
   )
 
-{placed_comp.labels}
-{placed_comp.symbol}
+{placed_comp.rendered_labels}
+{placed_comp.rendered_symbol}
 
   (sheet_instances
     (path "/" (page ""))
   )
 
   (symbol_instances
-{placed_comp.symbol_instance}
+{placed_comp.rendered_symbol_inst}
   )
 )
 '''
--- nl2sch.py
+++ nl2sch.py
@@ -9,26 +9,7 @@ import argparse
 from typing import DefaultDict
 
 from net import Netlist
-from comp import PlacedSchComponent, SchComponent
-
-# Empty schematic
-"""
-(kicad_sch (version 20201015) (generator eeschema)
-
-  (paper "A4")
-
-  (lib_symbols
-  )
-
-
-  (sheet_instances
-    (path "/" (page ""))
-  )
-
-  (symbol_instances
-  )
-)
-"""
+from comp import MatchedSchComponent, PlacedSchComponent, SchComponent
 
 def main(arguments):
 
@@ -53,6 +34,18 @@ def main(arguments):
         '-s', '--skip-missing',
         help='Skip NetComponent with no SchComponent matches',
         action='store_true'
+    )
+    parser.add_argument(
+        '--width',
+        help='Maximum width of a component group in schematic',
+        type=int,
+        default=450
+    )
+    parser.add_argument(
+        '--spacing',
+        help='Spacing between groups of components',
+        type=int,
+        default=10
     )
 
     args = parser.parse_args(arguments)
@@ -82,94 +75,101 @@ def main(arguments):
     # TODO - do a simple placing algorithm that groups components based on common nets
     #        (select a central component, and the algo places everything around it)
 
-    # Currently we just dump the components by descending pin count
-    net_comps = sorted(netlist.comps.values(), key = lambda c : len(c.connections), reverse=True)
+    # Phase 1 - match & collect
+    no_skipped = 0
 
-    # Last x, y for new SchComponents
-    last_x, last_y = 0,0
-    # Schematic bounds
-    max_x, max_y = 0,0
+    all_matched_comps : dict[SchComponent, list[MatchedSchComponent]] = DefaultDict(list)
 
-    placed_comps: dict[SchComponent : list[PlacedSchComponent]] = DefaultDict(list)
-
-    for net_comp in net_comps:
+    for net_comp in netlist.comps.values():
         net_comp_str = f'[{net_comp.designator} {net_comp.footprint} {net_comp.value}]'
 
         # Find first SchComponent that can match the D, F, V of net_comp
         for sch_comp in sch_comps:
-            if(sch_comp.match(net_comp.designator, net_comp.footprint, net_comp.value)):
-                
-                placed_comps_grp: list[PlacedSchComponent] = placed_comps[sch_comp]
-
-                # If we matched this SchComponent group before, place it in the same row
-                #
-                # TODO: KiCad doesn't like big schematics, so implement spilling of same group components
-                #       into next row - this means though that we first need to collect the components,
-                #       determine the bounds of the whole group, and _then_ place them
-                #
-                #       Phase 1 - detect & collect
-                #       Phase 2 - place
-                #
-                if placed_comps_grp:
-                    place_x = placed_comps_grp[-1].x + placed_comps_grp[-1].bounds[0]
-                    place_y = placed_comps_grp[-1].y
-
-                # Otherwise, start new row
-                else:
-                    place_x = last_x
-                    place_y = last_y
-
-                    # Advance last x, y for new category
-                    last_y += sch_comp.bounds[1]
-
-                if place_x > max_x:
-                    max_x = place_x
-                
-                if place_y > max_y:
-                    max_y = place_y
-
-                placed_comp = sch_comp.place(place_x, place_y, net_comp.designator, net_comp.value, net_comp.connections)
-                placed_comps_grp.append(placed_comp)
-
-                print(f'PLACED {net_comp_str} as\n{placed_comp}\n')
-                
+            match = sch_comp.match(net_comp)
+            if match:
+                all_matched_comps[sch_comp].append(match)
                 break
         else:
             # We didn't match any sch_comp
             if args.skip_missing:
                 print(f'SKIPPING {net_comp_str}')
+                no_skipped += 1
             else:
                 print(f'ERROR: {net_comp_str} could not be mapped to any SchComponent')
                 sys.exit(1)
 
-    # Render placed components into KiCad schematic
-    all_placed = [pc for pc_list in placed_comps.values() for pc in pc_list]
+    if args.skip_missing:
+        print(f'Skipped {no_skipped} netlist components.')
 
-    lib_symbol = '\n'.join([p.lib_symbol for p in all_placed])
-    labels = '\n'.join([p.labels for p in all_placed])
-    symbol = '\n'.join([p.symbol for p in all_placed])
-    symbol_instance = '\n'.join([p.symbol_instance for p in all_placed])
+    # Phase 2 - place
+
+    # Dump the components by descending pin count
+    all_matched_comps_sorted = sorted(
+        all_matched_comps.items(),
+        key = lambda m : len(m[0].label_tpls),
+        reverse=True
+    )
+    all_placed_comps : list[PlacedSchComponent] = []
+
+    x, y = 0,0
+    max_x = 0
+
+    for sch_comp, matched_comps in all_matched_comps_sorted:
+        for matched_comp in matched_comps:
+
+            all_placed_comps.append(matched_comp.place((x,y)))
+
+            new_x = x + sch_comp.bounds[0]
+            new_y = y + sch_comp.bounds[1]
+
+            # If the next component would be outside specified bounds,
+            # we will place it in the next row
+            if new_x > args.width:
+                x = 0
+                y = new_y
+            else:
+                # otherwise move within the row
+                x = new_x
+            
+            # Expand schematic bounds, if necessary
+            if new_x > max_x:
+                max_x = new_x
+
+        else:
+            if x:
+                # If we're currently in a non-empty row, move into new row
+                y = y + sch_comp.bounds[1]
+            x = 0
+            y += args.spacing
+
+    # Render placed components into KiCad schematic
+
+    rendered_lib_symbol = '\n'.join([p.lib_symbol for p in all_matched_comps.keys()])
+
+    rendered_labels = '\n'.join([p.rendered_labels for p in all_placed_comps])
+    rendered_symbol = '\n'.join([p.rendered_symbol for p in all_placed_comps])
+    rendered_symbol_instance = '\n'.join([p.rendered_symbol_inst for p in all_placed_comps])
 
     kicad_sch = open(args.kicad_sch_path, 'w')
     kicad_sch.write(
 f'''
 (kicad_sch (version 20201015) (generator eeschema)
 
-  (paper "User" {max_x} {max_y})
+  (paper "User" {max_x} {y})
 
   (lib_symbols
-{lib_symbol}
+{rendered_lib_symbol}
   )
 
-{labels}
-{symbol}
+{rendered_labels}
+{rendered_symbol}
 
   (sheet_instances
     (path "/" (page ""))
   )
 
   (symbol_instances
-{symbol_instance}
+{rendered_symbol_instance}
   )
 )
 '''
